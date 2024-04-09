d04814cb09a9562ae3d5e9a1329e2d8236bbf293
a10adotapp <mail@a10a.app>
2024-04-09 14:21:32 +0900

    /upload を追加

--- router/router.go
+++ router/router.go
@@ -1,7 +1,12 @@
 package router
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/a10adotapp/a10a.app/ent"
@@ -34,6 +39,82 @@ func NewRouter(entClient *ent.Client) *chi.Mux {
 	router.Route("/finschia", FinschiaRoute(finschia.NewFinschiaService(entClient)))
 	router.Route("/kusogeeeeeenft", KusogeeeeeeNFTRoute(kusogeeeeeenft.NewKusogeeeeeeNFTService(entClient)))
 	router.Route("/line-nft", LINENFTRoute(linenft.NewLINENFTService(entClient)))
+
+	router.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
+		file, fileHeader, err := r.FormFile("file")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("{\"message\":\"%s\"}\n", err.Error())))
+			return
+		}
+
+		defer file.Close()
+
+		dirname := filepath.Join(os.Getenv("UPLOADED_FILE_DIR"), time.Now().Format("20060102"))
+		if len(dirname) == 0 {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("{\"message\":\"no uploaded file directory specified\"}\n"))
+			return
+		}
+
+		if err := os.MkdirAll(dirname, os.ModePerm); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("{\"message\":\"%s\"}\n", err.Error())))
+			return
+		}
+
+		filePath := filepath.Join(dirname, fileHeader.Filename)
+
+		fileInfo, err := os.Stat(filePath)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("{\"message\":\"%s\"}\n", err.Error())))
+			return
+		}
+
+		if fileInfo != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("{\"message\":\"file already exists\"}\n"))
+			return
+		}
+
+		fileToSave, err := os.Create(filePath)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("{\"message\":\"%s\"}\n", err.Error())))
+			return
+		}
+
+		defer fileToSave.Close()
+
+		offset := int64(0)
+
+		for {
+			buf := make([]byte, 100)
+			n, err := file.Read(buf)
+			buf = buf[:n]
+			if err == io.EOF {
+				buf = buf[:n]
+			}
+
+			if n == 0 {
+				break
+			}
+
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(fmt.Sprintf("{\"message\":\"%s\"}\n", err.Error())))
+				return
+			}
+
+			fileToSave.WriteAt(buf, offset)
+
+			offset += int64(n)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("{\"message\":\"OK\"}\n"))
+	})
 
 	return router
 }
